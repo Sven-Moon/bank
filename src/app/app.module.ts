71d58696da24709be77ac66c1f29170b35170b2b
Sventropy <svenpire@gmail.com>
2020-12-14 11:38:31 -0500

    store set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './components/app/app-routing.module';
+import { AppComponent } from './components/app/app.component';
+import { AppComponents } from './components/app-components';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    ...AppComponents
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [], 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/app-components.ts
@@ -0,0 +1,7 @@
+import { AccountsComponent } from './accounts/accounts.component';
+import { AppComponent } from './app/app.component';
+
+export const AppComponents = [ 
+  AppComponent, 
+  AccountsComponent
+]
